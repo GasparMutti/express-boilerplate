34271edb07a4b58cf7d3a5bcb0ba54b705e20b59
GasparWindows <gasparrobortellamutti@gmail.com>
2024-11-17 21:30:05 -0300

    fix/match environment variable name

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
   port: process.env.PORT || 8080,
   accessControlAllowOrigins:
-    process.env.ACCESS_CONTROL_ALLOW_ORIGINGS || "http:localhost:9090",
+    process.env.ALLOWED_ORIGINS || "http:localhost:9090",
 };
